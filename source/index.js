82a5d5859b0897cdc468636ee9f08d7b450c6bbc
aumanything <aum.anything@gmail.com>
2024-07-18 13:44:28 +0700

    Delete source directory

--- source/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import SurveyCreatorRenderComponent from "./SurveyCreatorComponent";
-
-const root = createRoot(document.getElementById("surveyCreatorContainer"));
-root.render(<SurveyCreatorRenderComponent />);
